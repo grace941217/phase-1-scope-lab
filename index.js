0785a75b9658f8c0653b67cfe02670bb8772c06d
Grace <grace941217@gmail.com>
2022-09-20 23:43:18 -0400

    commit tree 54395a514b030e8da787afaeb34d41ddff25935b

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    return window.customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'Rose'
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'bob'
+}
